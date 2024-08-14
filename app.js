0db470523c6aef9346a94ff49eb94d19223e9808
johnatanf <johnatan.f100@gmail.com>
2024-08-14 13:25:18 +0700

    implement cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const registerRoutes = require("./routes/register.js");
 const loginRoutes = require("./routes/login.js");
@@ -17,6 +18,7 @@ const errorHandler = (err, req, res, next) => {
   }
 };
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
