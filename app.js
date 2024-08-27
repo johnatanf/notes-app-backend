4d53a0a7974d0beb8bde10219e3fc5e1f0b0a752
johnatanf <johnatan.f100@gmail.com>
2024-08-27 11:34:07 +0700

    fix cors

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ const errorHandler = (err, req, res, next) => {
   }
 };
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
