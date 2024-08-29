fa18600f806e11b5ed54628daa5017bf6b93d668
johnatanf <johnatan.f100@gmail.com>
2024-08-29 14:21:47 +0700

    set up for heroku deployment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -6,7 +7,7 @@ const loginRoutes = require("./routes/login.js");
 const notesRoutes = require("./routes/notes.js");
 const authRoutes = require("./routes/auth.js");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
@@ -19,9 +20,18 @@ const errorHandler = (err, req, res, next) => {
   }
 };
 
+const getCorsOrigin = () => {
+  const origin = process.env.FRONTEND_ORIGIN;
+  if (origin === undefined || origin === '') {
+    // Default to localhost if FRONTEND_ORIGIN is undefined or empty
+    return 'http://localhost:5173';
+  }
+  return origin;
+};
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: getCorsOrigin(),
     credentials: true,
   })
 );
@@ -39,5 +49,5 @@ app.all("*", (req, res) => res.send("This route does not exist."));
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`starting http://localhost:${port}`);
+  console.log(`starting port:${port}`);
 });
