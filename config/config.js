018394ad827f5a9af809935b8daff1ecf4a2e22b
johnatanf <johnatan.f100@gmail.com>
2024-08-29 12:37:50 +0700

    modify configuration for development, test and production

--- config/config.js
+++ config/config.js
@@ -2,24 +2,27 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOSTNAME,
+    "username": process.env.DB_USERNAME_DEVELOPMENT,
+    "password": process.env.DB_PASSWORD_DEVELOPMENT,
+    "database": process.env.DB_NAME_DEVELOPMENT,
+    "host": process.env.DB_HOSTNAME_DEVELOPMENT,
+    "port": process.env.DB_PORT_DEVELOPMENT,
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USERNAME_TEST,
+    "password": process.env.DB_PASSWORD_TEST,
+    "database": process.env.DB_NAME_TEST,
+    "host": process.env.DB_HOSTNAME_TEST,
+    "port": process.env.DB_PORT_TEST,
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USERNAME_PRODUCTION,
+    "password": process.env.DB_PASSWORD_PRODUCTION,
+    "database": process.env.DB_NAME_PRODUCTION,
+    "host": process.env.DB_HOSTNAME_PRODUCTION,
+    "port": process.env.DB_PORT_PRODUCTION,
     "dialect": "postgres"
   }
 }
